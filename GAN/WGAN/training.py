db56be58bfccc8d41f8a499f951eb8a19df77e1e
karthik <rskarthik96@gmail.com>
2021-06-30 16:21:31 +0530

    bug Fixes, moved entire dataset to GPU

--- GAN/WGAN/training.py
+++ GAN/WGAN/training.py
@@ -46,12 +46,14 @@ def training(opt):
     # ~~~~~~~~~~~~~~~~~~~ loading the dataset ~~~~~~~~~~~~~~~~~~~ #
 
     trans = transforms.Compose(
-        [transforms.ToPILImage(), transforms.Resize((H, W)),
+        [transforms.Resize((H, W)),
          transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
     MNIST_data = MNIST('/datasets', True, transform=trans, download=True)
+    MNIST_data = MNIST_data.to(work_device)
 
-    loader = DataLoader(MNIST_data, BATCH_SIZE, True, num_workers=1)
+    loader = DataLoader(MNIST_data, BATCH_SIZE, True,
+                        num_workers=1, pin_memory=True)
 
     # ~~~~~~~~~~~~~~~~~~~ creating tensorboard variables ~~~~~~~~~~~~~~~~~~~ #
 
@@ -95,7 +97,7 @@ def training(opt):
         for batch_idx, (real, _) in enumerate(tqdm(loader)):
             critic.train()
             gen.train()
-            real = real.to(work_device)
+            # real = real.to(work_device)
             fixed_noise = torch.rand(
                 real.shape[0], Z_DIM, 1, 1).to(work_device)
 
